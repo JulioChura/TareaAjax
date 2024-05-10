03d2e0efd942312a7e50218ae4f70744167bfe82
Julio Chura <jchuraaca@unsa.edu.pe>
2024-05-10 18:55:48 -0500

    Se corrige el error y era la ubicacion del archivo, ademas se cambia fetch por ajax

--- problema1/javascript/script.js
+++ problema1/javascript/script.js
@@ -1,5 +1,14 @@
-const url = "data.json";
-fetch(url)
-    .then(response => response.json()) 
-    .then(data => console.log(data))
-    
+console.log("hola")
+function getData() {
+    console.log("desde ajax")
+    const url = "../data.json";
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', url, true);
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText)
+        }
+    }
+}
+getData();
