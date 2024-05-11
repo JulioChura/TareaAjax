2939bc87af100716265f4fe818104527079351cd
Julio Chura <jchuraaca@unsa.edu.pe>
2024-05-11 10:33:58 -0500

    Se establece las opciones para cada opcion de select

--- problema1/javascript/script.js
+++ problema1/javascript/script.js
@@ -8,8 +8,30 @@ document.addEventListener('DOMContentLoaded', function() {
         if (this.readyState == 4 && this.status == 200) {
             data = JSON.parse(this.response);
             console.log(data);
-                
+            let section1 = document.getElementById('regions1');
+            let section2 = document.getElementById('regions2');
+            section1.innerHTML = showRegions();
+            section2.innerHTML = showRegions();
+            
         }
     }
 })
+
+function showRegions() {
+    let body = "<option selected>Open this select menu</option>"
+    for(item of data) {
+        body += `<option value="1">´${item.region}</option>`;
+    }
+    return body;
+}
+
 console.log(data);
+
+
+
+
+
+
+function mostrarGrafico(datos) {
+
+}
