8ef4a6bb9c3fc64a5d428e32cbace0d8ad6f7011
Julio Chura <jchuraaca@unsa.edu.pe>
2024-05-11 14:42:07 -0500

    Se decide canrgar los datos del json y guardarlos, esto ocurre durante la carga del documento

--- problema1/javascript/script.js
+++ problema1/javascript/script.js
@@ -1,23 +1,14 @@
-function getData() {
+let data;
+document.addEventListener('DOMContentLoaded', function() {
     const url = "../data.json";
     const xhttp = new XMLHttpRequest();
     xhttp.open('GET', url, true);
     xhttp.send();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            let datos = JSON.parse(this.response);
-            console.log(datos);
-        for(let item of datos) {
-            console.log(item.region);
-        }
+            data = JSON.parse(this.response);
+            console.log(data);
+                
         }
     }
-}
-
-function listaRegiones(data) {
-    let body = "<option selected>Open this select menu</option>"
-    for(item of data) {
-        body += `<option value="1">´${item.region}</option>`;
-    }
-}
-getData();
+})
