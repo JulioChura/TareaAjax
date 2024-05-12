00951ec50277f047cb4f03c29caa62cb64eef4d7
Julio Chura <jchuraaca@unsa.edu.pe>
2024-05-11 22:28:39 -0500

    Se modfica la funcion showGraphic para que muestre una grafica con todas las regiones

--- problema2/javascript/script.js
+++ problema2/javascript/script.js
@@ -7,22 +7,33 @@ document.addEventListener('DOMContentLoaded', function() {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             data = JSON.parse(this.response);
-            console.log(data);
-            let section1 = document.getElementById('regions1');
-            let section2 = document.getElementById('regions2');
-            section1.innerHTML = showRegions();
-            section2.innerHTML = showRegions();
+            console.log(data)
+            showGraphic(1,1)
         }
     }
 })
 
-function showGraphic(value, idBox) {
-    let graf = document.getElementById("box"+idBox);
-    graf.innerHTML = `<canvas id='${idBox}'  class='opciones__lado' width='570'></canvas>`;
+function showGraphic( idBox) {
     let box = document.getElementById(idBox);
     let date = [];
-    let info = [];
-    for(let i = 10, index = 0; i < (data[value].confirmed.length); i=i+3, index++) {
+
+    let dictionay = [];
+    for(let index = 0; index < data.length; index++) {
+        let temDic;
+        temDic ={label: data[index].region}
+        let values = [];
+        for (let i = 6, indice = 0; indice < 65/3; i=i+2, indice++) {
+            console.log(data[index].confirmed[i].value)
+            values[indice] = data[index].confirmed[i].value;
+        }
+        temDic.data = values;
+        dictionay[index] = temDic;
+    }
+    console.log("holas")
+    console.log(dictionay);
+    
+
+    for(let i = 10, index = 0; i < (data[value].confirmed.length); i=i+2, index++) {
         date[index] = data[value].confirmed[i].date;
         info[index] = data[value].confirmed[i].value;
     }
@@ -32,20 +43,15 @@ function showGraphic(value, idBox) {
             labels: date,
             datasets: [{
                 label: "date",
-                data: info,
-            }]
+                data:  info
+            },
+            {
+                label: "daqte",
+                data:  info
+            }    
+            ]
         }
     })
-}
-
-function showRegions() {
-    let index = 0;
-    let body = '<option selected class="options">Open this select menu</option>'
-    for(item of data) {
-        body += `<option class="options" value="${index}">${item.region}</option>`;
-        index++;
-    }
-    return body;
 }
 
 
