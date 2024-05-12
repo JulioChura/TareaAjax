ffec38fd76b98aa5d72e33b55edcb43a4c01cc7d
Julio Chura <jchuraaca@unsa.edu.pe>
2024-05-11 23:32:53 -0500

    Se corrige el id del canva, ademas se crea un diccionario que almacena los datos necesarios

--- problema2/javascript/script.js
+++ problema2/javascript/script.js
@@ -8,16 +8,18 @@ document.addEventListener('DOMContentLoaded', function() {
         if (this.readyState == 4 && this.status == 200) {
             data = JSON.parse(this.response);
             console.log(data)
-            showGraphic(1,1)
+            showGraphic()
         }
     }
 })
 
-function showGraphic( idBox) {
-    let box = document.getElementById(idBox);
-    let date = [];
+function showGraphic() {
+    let box = document.getElementById('lienzo');
+    let fechas = [];
 
-    let dictionay = [];
+    let nameRegion = [];
+    
+    let dictionary = [];
     for(let index = 0; index < data.length; index++) {
         let temDic;
         temDic ={label: data[index].region}
@@ -25,31 +27,24 @@ function showGraphic( idBox) {
         for (let i = 6, indice = 0; indice < 65/3; i=i+2, indice++) {
             console.log(data[index].confirmed[i].value)
             values[indice] = data[index].confirmed[i].value;
+            fechas[indice]  = data[index].confirmed[i].date;
         }
         temDic.data = values;
-        dictionay[index] = temDic;
+        dictionary[index] = temDic;
+        date = data[index].region;
     }
-    console.log("holas")
-    console.log(dictionay);
-    
+    console.log(dictionary[3])
+    console.log(fechas)
 
-    for(let i = 10, index = 0; i < (data[value].confirmed.length); i=i+2, index++) {
-        date[index] = data[value].confirmed[i].date;
-        info[index] = data[value].confirmed[i].value;
-    }
     const chartjs = new Chart(box, {
         type: 'line',
         data: {
-            labels: date,
+            labels: fechas,
+            
             datasets: [{
-                label: "date",
-                data:  info
-            },
-            {
-                label: "daqte",
-                data:  info
-            }    
-            ]
+                label: "hola",
+                data: [2,3,4,3,2,1]
+            }]
         }
     })
 }
