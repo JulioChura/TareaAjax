e261a4b7bce3fc9be2020d72ac11c1baf70393ef
Julio Chura <jchuraaca@unsa.edu.pe>
2024-05-11 23:51:15 -0500

    Se borran los console.log y el ejercicio 2 se ha terminado

--- problema2/javascript/script.js
+++ problema2/javascript/script.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', function() {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             data = JSON.parse(this.response);
-            console.log(data)
             showGraphic()
         }
     }
@@ -15,17 +14,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function showGraphic() {
     let box = document.getElementById('lienzo');
-    let fechas = [];
-
-    let nameRegion = [];
-    
+    let fechas = [];    
     let dictionary = [];
     for(let index = 0; index < data.length; index++) {
         if (data[index].region !== 'Lima' && data[index].region !== 'Callao') {
             let temDic = { label: data[index].region };
             let values = [];
-
-            for (let i = 6, indice = 0; indice < 65 / 3; i = i + 2, indice++) {
+            for (let i = 6, indice = 0; indice < 65 / 3; i++, indice++) {
                 console.log(data[index].confirmed[i].value);
                 values[indice] = data[index].confirmed[i].value;
                 fechas[indice] = data[index].confirmed[i].date;
@@ -35,9 +30,6 @@ function showGraphic() {
             date = data[index].region;
         }
     }
-    console.log(dictionary[3])
-    console.log(fechas)
-
     const chartjs = new Chart(box, {
         type: 'line',
         data: {
